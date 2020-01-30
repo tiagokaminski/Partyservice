5d9d2aac306d3d354db61b13c77019537a83a27d
Tom Becker <beckertom11@gmail.com>
2020-01-30 08:23:46 +0100

    Datenbanken

--- src/Panelvorlage/Wilkommensscreen.java
+++ src/Panelvorlage/Wilkommensscreen.java
@@ -6,7 +6,6 @@ import javax.swing.JTextField;
 import java.awt.Font;
 
 public class Wilkommensscreen extends Panelvorlage {
-	private JLabel labelWilkommen;
 	private JLabel labelPartyservice;
 	private JLabel labelVonAndreasTom;
 
@@ -19,15 +18,9 @@ public class Wilkommensscreen extends Panelvorlage {
 	}
 	private void initGUI() {
 		{
-			labelWilkommen = new JLabel("Wilkommen beim ");
-			labelWilkommen.setFont(new Font("Tahoma", Font.PLAIN, 30));
-			labelWilkommen.setBounds(84, 46, 265, 26);
-			add(labelWilkommen);
-		}
-		{
 			labelPartyservice = new JLabel("Partyservice");
 			labelPartyservice.setFont(new Font("Tahoma", Font.PLAIN, 30));
-			labelPartyservice.setBounds(109, 93, 167, 26);
+			labelPartyservice.setBounds(134, 11, 169, 43);
 			add(labelPartyservice);
 		}
 		{
@@ -35,5 +28,9 @@ public class Wilkommensscreen extends Panelvorlage {
 			labelVonAndreasTom.setBounds(0, 207, 453, 26);
 			add(labelVonAndreasTom);
 		}
+		
+		JLabel lblSmiley = new JLabel("");
+		lblSmiley.setBounds(141, 151, 46, 14);
+		add(lblSmiley);
 	}
 }
